06bb61bf60e3fd486c21954507eb1153f6782807
Diego Reis <diegoreso@me.com>
2023-08-07 17:47:42 -0300

    create rent model

---
+++ Aluguel/Models/EmprestimoModels.cs
@@ -0,0 +1,15 @@
+﻿namespace Aluguel.Models
+{
+    public class EmprestimoModels
+    {
+        public int Id { get; set; }
+
+        public string Recebedor { get; set; }
+
+        public string Fornecedor { get; set; }
+
+        public int FitaAlugada { get; set; }
+
+        public DateTime DataAluguel { get; set; } = DateTime.Now;
+    }
+}
