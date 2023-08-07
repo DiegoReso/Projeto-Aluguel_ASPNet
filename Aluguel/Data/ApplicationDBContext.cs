ee56911bafc9509a0d08c964d962ce8be9914422
Diego Reis <diegoreso@me.com>
2023-08-07 18:11:09 -0300

    create rent migration

---
+++ Aluguel/Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Aluguel.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Aluguel.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        
+        }
+
+
+        public DbSet<EmprestimoModels> Emprestimos { get; set; }
+
+    }
+}
---
+++ Aluguel/Migrations/20230807210751_CriacaoDoBanco.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aluguel.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoDoBanco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Emprestimos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Recebedor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fornecedor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FitaAlugada = table.Column<int>(type: "int", nullable: false),
+                    DataAluguel = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emprestimos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Emprestimos");
+        }
+    }
+}
