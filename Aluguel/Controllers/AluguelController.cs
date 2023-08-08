ead523036c57945d3ba94744f1a96c99bbfe8f4e
Diego Reis <diegoreso@me.com>
2023-08-08 15:16:07 -0300

    create rent view

---
+++ Aluguel/Controllers/AluguelController.cs
@@ -0,0 +1,24 @@
+﻿using Aluguel.Data;
+using Aluguel.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aluguel.Controllers
+{
+    public class AluguelController : Controller
+    {
+
+        readonly private ApplicationDBContext _db;
+
+        public AluguelController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimoModels> emprestimos = _db.Emprestimos;
+            return View(emprestimos);  
+        }
+    }
+}
